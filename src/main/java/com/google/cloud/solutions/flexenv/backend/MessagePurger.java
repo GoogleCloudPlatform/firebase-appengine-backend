238ce15f04a361ea31df2ff6c0e6c1a46ea45245
Xiang Shen <shenxiang@google.com>
2020-09-02 00:21:06 -0400

    update dependencies

--- src/main/java/com/google/cloud/solutions/flexenv/backend/MessagePurger.java
+++ src/main/java/com/google/cloud/solutions/flexenv/backend/MessagePurger.java
@@ -19,7 +19,6 @@ package com.google.cloud.solutions.flexenv.backend;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
